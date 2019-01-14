8843a44dc5721b73c56a92d09e14d97c1f7b58a3
ace411 <lochbm@live.com>
2019-01-14 11:26:11 +0300

    Modified console functions

--- src/Console/Console.php
+++ src/Console/Console.php
@@ -25,129 +25,63 @@ function execCmd(string $cmd) : IO
     return patternMatch(
         [
             '["cred", type, user, pass]' => function (string $type, string $user, string $pass) {
-                return configWrite(function (array $config) use ($user, $pass, $type) {
+                return _writeConfig(function (array $config) use ($type, $user, $pass) : array {
+                    if (!isset($config['username'][$type]) && !isset($config['password'][$type])) {
+                        return $config;
+                    }
                     $config['username'][$type] = $user;
                     $config['password'][$type] = $pass;
-                    $res = A\compose(A\partialRight('json_encode', \JSON_PRETTY_PRINT), IO\IO);
-                    return $res($config);
-                });
+                    return $config;
+                }, A\partialRight(_writeMsg, 'Credentials not updated', 'Credentials updated'));
             },
             '["use", option]' => function (string $option) {
-                return configWrite(function (array $config) use ($option) {
-                    $config['local'] = patternMatch(
-                        [
-                            '"local"' => function () {
-                                return true;
-                            },
-                            '_' => function () {
-                                return false;
-                            }
-                        ],
-                        $option
-                    );
-                    $res = A\compose(A\partialRight('json_encode', \JSON_PRETTY_PRINT), IO\IO);
-                    return $res($config);
-                });
+                return _writeConfig(function (array $config) use ($option) {
+                    $config['local'] = $option == 'local' ? true : false;
+                    return $config;
+                }, A\partialRight(_writeMsg, 'Configuration not updated', 'Configuration updated'));
             },
             '["gzip", database, file]' => function (string $database, string $file) {
-                $zip = M\mcompose(function (string $contents) use ($file) {
-                    $check = function (array $data) : array {
-                        return isset($data['rows']) ? A\pluck($data, 'rows') : A\identity([]);
-                    };
-                    $final = A\compose(A\partialRight('json_decode', true), $check, 'json_encode');
-                    $resource = \gzopen($file, 'w9');
-                    \gzwrite($resource, $final($contents));
-                    return IO\IO(\gzclose($resource) ? color('Success', 'green') : color('Failure', 'red'));
-                }, A\partialRight(Actions\allDocs, ['include_docs' => 'true']));
-
-                return $zip(IO\IO($database));
+                $gzip = A\compose(
+                    A\partialRight(_gzDoc, $file),
+                    A\partialRight(_writeMsg, 'Data not zipped', A\concat('', $file, '.gz created'))
+                );
+                return $gzip($database);
             },
             '["unzip", file]' => function (string $file) {
-                $read = A\compose(
-                    'gzfile', 
-                    A\partialRight(A\pluck, 0),  
-                    A\partialRight('json_decode', true),
-                    A\partialRight('json_encode', \JSON_PRETTY_PRINT),
-                    formatOutput
-                );
-                return $read($file);
+                return _gzDocUnzip($file);
             },
             '["search", database, selector]' => function (string $database, string $selector) {
-                return configRead(function ($contents) use ($selector, $database) {
-                    $res = A\compose(
-                        A\partialRight('json_decode', true),
-                        A\partialRight(A\pluck, 'console'),
-                        A\partialRight(A\pluck, 'search'),
-                        A\partialLeft(A\extend, ['selector' => \json_decode($selector, true)]),
-                        A\partial(Actions\search, $database)
-                    );
-
-                    return M\bind(function (string $docs) {
-                        $res = A\compose(
-                            A\partialRight('json_decode', true), 
-                            A\partialRight(A\pluck, 'docs'), 
-                            A\partialRight('json_encode', \JSON_PRETTY_PRINT),
-                            formatOutput
-                        );
-                        return $res($docs);
-                    }, $res($contents));
-                });
+                return _search($database, $selector);
             },
             '["alldocs", database]' => function (string $database) {
-                return configRead(function (string $contents) use ($database) {
-                    $res = A\compose(
-                        A\partialRight('json_decode', true),
-                        A\partialRight(A\pluck, 'console'),
-                        A\partialRight(A\pluck, 'alldocs')
-                    );
-
-                    return M\bind(function ($contents) {
-                        $res = A\compose(
-                            A\partialRight('json_decode', true), 
-                            A\partialRight('json_encode', \JSON_PRETTY_PRINT),
-                            formatOutput
-                        );
-                        return $res($contents);
-                    }, Actions\allDocs($database, $res($contents)));
-                });
+                return _multipleDocs($database);
             },
             '["db", database]' => function (string $database) {
-                return outputAction(Actions\database($database));
+                return;                
+            },
+            '["show", opt]' => function (string $opt) {
+                return _printConfig($opt);
             },
             '["uuids", count]' => function (string $count) {
-                return outputAction(Actions\uuids(is_numeric($count) ? (int) $count : 1));
+                return _uuids((int) $count);
             },
             '["doc", database, docId]' => function (string $database, string $docId) {
-                return outputAction(Actions\doc($database, $docId, array('include_docs' => 'true')));
+                return _singleDoc($database, $docId);
+            },
+            '["docs", database, keys]' => function (string $database, string $keys) {
+                return _keyDocs($database, $keys);
             },
             '["explain", cmd]' => function (string $cmd) {
-                $res = A\compose(
-                    A\partialRight(A\pluck, $cmd),
-                    A\identity('array_values'),
-                    A\partialRight('json_encode', \JSON_PRETTY_PRINT),
-                    formatOutput
-                );
-                return \key_exists($cmd, State::CONSOLE_COMMANDS) ? 
-                    $res(State::CONSOLE_COMMANDS) :
-                    IO\IO(color(A\concat(' ', $cmd, 'not supported'), 'yellow'));
+                return _explain($cmd);
             },
             '["alldbs"]' => function () {
-                return outputAction(Actions\allDbs());
+                return _databases();
             },
             '["help"]' => function () {
-                $res = A\compose(function (array $cmd) {
-                    $out = \array_map(function ($key, $val) {
-                        return A\concat(': ', $key, A\pluck($val, 'desc')); 
-                    }, \array_keys($cmd), \array_values($cmd));
-                    return \json_encode($out, \JSON_PRETTY_PRINT);
-                }, formatOutput);
-
-                return $res(State::CONSOLE_COMMANDS);
+                return _allCmds();
             },
-            '["config"]' => function () {
-                return configRead(function ($contents) {
-                    return formatOutput($contents);
-                });
+            '["show", cmd]' => function (string $cmd) {
+                return _explain($cmd);
             },
             '["exit"]' => function () {
                 M\bind(function (string $msg) {
@@ -157,89 +91,10 @@ function execCmd(string $cmd) : IO
                 exit();
             },
             '_' => function () {
-                $output = A\compose(A\partialRight(color, 'red'), IO\IO);
+                $output = A\compose(A\partialRight(_style, 'red'), IO\IO);
                 return $output('Input not recognized');
             }
         ],
         explode(' ', $cmd)
     );
-}
-
-function configRead(callable $action) : IO
-{
-    return M\bind($action, Http\_readConfig());
-}
-
-function configWrite(callable $action) : IO
-{
-    return configRead(function (string $contents) use ($action) {
-        $result = M\mcompose(A\partial(IO\writeFile, Http\_configPath()), $action);
-        return M\bind(function (int $result) {
-            return IO\IO(
-                $result > 0 ? 
-                    color('Credentials updated', 'green') : 
-                    color('Credentials not updated', 'red')
-            );
-        }, $result(IO\IO(json_decode($contents, true))));
-    });
-}
-
-function replPrompt() : IO
-{
-    $prompt = M\mcompose(
-        function ($prompt) {
-            $action = A\compose(A\partial('printf', '%s'), IO\IO);
-            return $action($prompt);
-        }, 
-        function (array $contents) {
-            $result = A\compose(A\partialRight(A\pluck, 'prompt'), IO\IO);
-            return $result($contents);
-        }    
-    );
-
-    return $prompt(IO\IO(State::CONSOLE_FEATURES));
-}
-
-const formatOutput = 'Chemem\\Fauxton\\Console\\formatOutput';
-
-function formatOutput(string $contents) : IO
-{
-    $format = A\compose(
-        A\partial('str_replace', '{', \implode('.', \array_fill(0, 3, '.'))),
-        A\partial('str_replace', '}', \implode('.', \array_fill(0, 3, '.'))),
-        A\partial('str_replace', '[', \implode('.', \array_fill(0, 3, '.'))),
-        A\partial('str_replace', ']', \implode('.', \array_fill(0, 3, '.'))),
-        A\partial('str_replace', '\\t', ' '),
-        A\partial('str_replace', '"', ''),
-        A\partial('str_replace', ',', ''),
-        A\partial('str_replace', '\\', ''),
-        IO\IO
-    );
-
-    return $format($contents);
-}
-
-const color = 'Chemem\\Fauxton\\Console\\color';
-
-function color(string $text, string $style = 'none')
-{
-    $color = new ConsoleColor;
-
-    return in_array($style, $color->getPossibleStyles()) && $color->isSupported() ?
-        $color->apply($style, $text) :
-        A\identity($text);
-}
-
-const outputAction = 'Chemem\\Fauxton\\Console\\outputAction';
-
-function outputAction(IO $action) : IO
-{
-    return M\bind(function ($contents) {
-        $res = A\compose(
-            A\partialRight('json_decode', true),
-            A\partialRight('json_encode', \JSON_PRETTY_PRINT),
-            formatOutput
-        );
-        return $res($contents);
-    }, $action);
 }
--- src/Console/parser.php
+++ src/Console/parser.php
@@ -7,3 +7,307 @@
  * @author Lochemem Bruno Michael
  * @license Apache-2.0
  */
+
+namespace Chemem\Fauxton\Console;
+
+use Chemem\Fauxton\Config\State;
+use \Chemem\Bingo\Functional\Algorithms as A;
+use \Chemem\Bingo\Functional\Functors\Monads as M;
+use \Chemem\Bingo\Functional\Functors\Monads\IO;
+use \Mmarica\DisplayTable;
+use \Chemem\Fauxton\{Http, Actions};
+use \JakubOnderka\PhpConsoleColor\ConsoleColor;
+use \Chemem\Bingo\Functional\PatternMatching as PM;
+
+const _replPrompt = 'Chemem\\Fauxton\\Console\\_replPrompt';
+function _replPrompt()
+{
+    return M\bind(function (string $prompt) {
+        $ret = A\compose(A\partial('printf', '%s'), IO\IO);
+        return $ret($prompt);
+    }, IO\IO(State::CONSOLE_PROMPT));
+}
+
+const _style = 'Chemem\\Fauxton\\Console\\_style';
+function _style(string $text, string $style = 'none') : string
+{
+    $color = new ConsoleColor;
+
+    return in_array($style, $color->getPossibleStyles()) && $color->isSupported() ?
+        $color->apply($style, $text) :
+        A\identity($text);
+}
+
+function _header() : callable
+{
+    return A\compose(A\partial(A\map, 'array_keys'), A\flatten, A\unique);
+}
+
+function _formatList(array $data) : string
+{
+    $format = A\compose(
+        A\toPairs, 
+        A\partial(A\map, A\partial('implode', ': ')), 
+        A\partial('implode', ', ')
+    );
+
+    return $format($data);
+}
+
+function _jsonRow(array $row) : array
+{
+    return A\map(function ($val) {
+        return !is_array($val) ? $val : _formatList($val);
+    }, $row);
+}
+
+const _row = 'Chemem\\Fauxton\\Console\\_row';
+function _row(array $keys, array $data) : array
+{
+    return A\map(function (array $data) use ($keys) {
+        $fill = A\fold(function ($acc, $key) use ($data) {
+            if (!isset($data[$key])) {
+                $acc[$key] = 'null';
+            }
+            return $acc;
+        }, $keys, array());
+        $rows = A\extend(_jsonRow($data), $fill);
+        ksort($rows);
+        return $rows;
+    }, $data);
+}
+
+const _headerRow = 'Chemem\\Fauxton\\Console\\_headerRow';
+function _headerRow(array $data) : array
+{
+    $headers = _header()($data);
+    asort($headers);
+    return array_values($headers);
+}
+
+function _dataRows(array $data) : array
+{
+    $sort = A\compose(_headerRow, A\partialRight(_row, $data), A\partial(A\map, 'array_values'));
+    return $sort($data);
+}
+
+function _table(callable $transform, array $data) : IO
+{
+    $ret = $transform($data);
+    return IO\IO(
+        DisplayTable::create()
+            ->headerRow(_headerRow($ret))
+            ->dataRows(_dataRows($ret))
+            ->toText()
+            ->mysqlBorder()
+            ->generate()
+    );
+}
+
+const _toConstant = 'Chemem\\Fauxton\\Console\\_toConstant';
+function _toConstant(IO $operation) : IO
+{
+    return M\bind(function ($result) {
+        return IO\IO(function () use ($result) {
+            return function () use ($result) {
+                return A\constantFunction($result);
+            };
+        });
+    }, $operation);
+}
+
+function _catchResponse(IO $response) : IO
+{
+    $catch = A\compose(_toConstant, IO\catchIO);
+    return $catch($response);
+}
+
+const _output = 'Chemem\\Fauxton\\Console\\_output';
+function _output(callable $action, IO $response) : IO
+{
+    return M\bind(function (callable $output) use ($action) {
+        $list = json_decode($output(), true);
+        return $list == null ? IO\IO($list) : _table($action, $list);
+    }, _catchResponse($response));
+}
+
+function _printConfig(string $opt) : IO
+{
+    return _output(function (array $config) use ($opt) {
+        $pluck = A\partial(A\pluck, $config);
+        return PM\patternMatch(array(
+            '"credentials"' => function () use ($pluck) {
+                return array(
+                    'username' => $pluck('username'),
+                    'password' => $pluck('password')
+                );
+            },
+            '"console"' => function () use ($pluck) {
+                return array($pluck('console'));
+            },
+            '"local"' => function () use ($pluck) {
+                return array(array('local' => $pluck('local')));
+            },
+            '"https"' => function () use ($pluck) {
+                return array('https' => $pluck('https'));
+            },
+            '_' => function () use ($config) {
+                return array(array('error' => 'Options are https, local, console, and credentials'));
+            }
+        ), $opt);
+    }, Http\_readConfig());
+}
+
+function _cliOpts(string $opt, array $merge = array()) : IO
+{
+    return M\bind(function (string $content) use ($opt, $merge) {
+        $config = A\compose(
+            A\partialRight('json_decode', true), 
+            A\partialRight(A\pluck, 'console'), 
+            A\partialRight(A\pluck, $opt),
+            A\partialRight(A\extend, $merge),
+            IO\IO
+        );
+        return $config($content);
+    }, Http\_readConfig());
+}
+
+function _singleColOutput(string $colName, IO $action) : IO
+{
+    return _output(function (array $data) use ($colName) : array {
+        return A\fold(function (array $acc, string $database) use ($colName) {
+            $acc[][$colName] = $database;
+            return $acc;
+        }, $data, array());
+    }, $action);
+}
+
+function _searchSelector(string $selector) : IO
+{
+    return _cliOpts('search', array(
+        'selector' => json_decode($selector, true)
+    ));
+}
+
+function _search(string $database, string $selector) : IO
+{
+    return M\bind(function (array $query) use ($database) {
+        $output = A\compose(
+            A\partial(Actions\search, $database),
+            A\partial(_output, A\partialRight(A\pluck, 'docs'))
+        );
+        return $output($query);
+    }, _searchSelector($selector));
+}
+
+function _databases() : IO
+{
+    return _singleColOutput('database', Actions\allDbs());
+}
+
+function _uuids(int $count) : IO
+{
+    return _singleColOutput('id', Actions\uuids($count));
+}
+
+function _singleDoc(string $database, string $docId) : IO
+{
+    return _output(function (array $doc) {
+        return array($doc);
+    }, M\bind(A\partial(Actions\doc, $database, $docId), _cliOpts('alldocs')));
+}
+
+function _multiple(callable $action) : IO
+{
+    return _output(function (array $docs) {
+        $ret = A\compose(A\partialRight(A\pluck, 'rows'), A\partial(A\map, A\partialRight(A\pluck, 'doc')));
+        return $ret($docs);
+    }, M\bind($action, _cliOpts('alldocs')));
+}
+
+function _keyDocs(string $database, string $keys) : IO
+{
+    return _multiple(A\partial(Actions\docKeys, $database, array('keys' => json_decode($keys, true))));
+}
+
+function _multipleDocs(string $database) : IO
+{
+    return _multiple(A\partial(Actions\allDocs, $database));
+}
+
+function _cmd() : callable
+{
+    return A\partialRight(_output, IO\IO(json_encode(State::CONSOLE_COMMANDS)));
+}
+
+function _allCmds() : IO
+{
+    return _cmd()(A\identity);
+}
+
+const _explain = 'Chemem\\Fauxton\\Console\\_explain';
+function _explain(string $cmd) : IO
+{
+    return !isset(State::CONSOLE_COMMANDS[$cmd]) ? 
+        IO\IO(_style('Command does not exist', 'yellow')) :
+        _cmd()(function (array $cmds) use ($cmd) {
+            return array(A\pluck($cmds, $cmd));
+        });
+}
+
+const _writeConfig = 'Chemem\\Fauxton\\Console\\_writeConfig';
+function _writeConfig(callable $transform, callable $printMsg) : IO
+{
+    return M\bind(function (string $content) use ($transform, $printMsg) {
+        $write = A\compose(
+            A\partialRight('json_decode', true),
+            $transform,
+            A\partialRight('json_encode', JSON_PRETTY_PRINT), 
+            A\partial(IO\writeFile, Http\_configPath()), 
+            $printMsg
+        );
+        return $write($content);
+    }, Http\_readConfig());
+}
+
+const _writeMsg = 'Chemem\\Fauxton\\Console\\_writeMsg';
+function _writeMsg(IO $result, string $success, string $failure) : IO 
+{
+    return M\bind(function (int $result) use ($success, $failure) {
+        return IO\IO($result > 0 ? $success : $failure);
+    }, $result);
+}
+
+const _gzip = 'Chemem\\Fauxton\\Console\\_gzip';
+function _gzip(string $file, string $content) : IO
+{
+    return IO\IO(function () use ($file, $content) {
+        $resource = gzopen($file, 'w9');
+        gzwrite($resource, $content);
+        return gzclose($resource);
+    });
+}
+
+const _gUnzip = 'Chemem\\Fauxton\\Console\\_gUnzip';
+function _gUnzip(string $file) : IO
+{
+    $unzip = A\compose('gzfile', A\partial('implode', ''), IO\IO);
+    return $unzip($file);
+}
+
+const _gzDoc = 'Chemem\\Fauxton\\Console\\_gzDoc';
+function _gzDoc(string $database, string $file) : IO
+{
+    return M\bind(
+        A\partial(_gzip, A\concat('.', $file, 'gz')), 
+        M\bind(A\partial(Actions\allDocs, $database), _cliOpts('alldocs'))
+    );
+}
+
+const _gzDocUnzip = 'Chemem\\Fauxton\\Console\\_gzDocUnzip';
+function _gzDocUnzip(string $file) : IO
+{
+    return _multiple(function () use ($file) : IO {
+        return _gUnzip(A\concat('.', $file, 'gz')); 
+    });
+}
