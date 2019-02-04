b8e2a85356ad732ae4a4f39dca2af99d82525236
ace411 <lochbm@live.com>
2019-02-04 16:23:13 +0300

    Added more tests

--- tests/ActionsTest.php
+++ tests/ActionsTest.php
@@ -31,11 +31,29 @@ class ActionsTest extends \PHPUnit\Framework\TestCase
         return A\toException('Clue\\React\\Block\\await');
     }
 
+    const ID_REGEX = '/([\w\d])+/';
+
+    const REV_REGEX = '/(\d){1}(-){1}([\w\d]*)/';
+
+    const specialConst = 'Chemem\\Fauxton\\Tests\\ActionsTest::specialConst';
+
+    const revConst = 'Chemem\\Fauxton\\Tests\\ActionsTest::revConst';
+
     const idConst = 'Chemem\\Fauxton\\Tests\\ActionsTest::idConst';
 
-    public static function idConst(string $val)
+    public static function specialConst(string $val, string $regex, int $limit)
     {
-        return preg_match('/([\w\d])+/', $val) && strlen($val) >= 32;
+        return preg_match($regex, $val) && strlen($val) >= $limit;
+    }
+
+    public static function idConst(string $val) : bool
+    {
+        return self::specialConst($val, self::ID_REGEX, 32);
+    }
+
+    public static function revConst(string $val) : bool
+    {
+        return self::specialConst($val, self::REV_REGEX, 34);
     }
 
     /**
@@ -179,6 +197,33 @@ class ActionsTest extends \PHPUnit\Framework\TestCase
             });
     }
 
+    /**
+     * @eris-repeat 5
+     */
+    public function testInsertMultiplePutsDataInDatabase()
+    {
+        $this->forAll(
+            Generator\constant('testdb'),
+            Generator\associative([
+                'docs' => Generator\tuple(
+                    Generator\associative([
+                        'user' => Generator\names() 
+                    ]),
+                    Generator\associative([
+                        'date' => Generator\date()
+                    ]),
+                )
+            ])
+        )
+            ->then(function (string $database, array $data) {
+                $promise = $this->action->insertMultiple($database, $data);
+                $insert = self::blockFn()($promise, $this->eventLoop);
+
+                $this->assertInstanceOf(\React\Promise\Promise::class, $promise);
+                $this->assertInternalType('string', $insert);
+            });
+    }
+
     public function testQueryParamsPrintsArrayOfUrlQueryParameters()
     {
         $this->forAll(
@@ -196,6 +241,190 @@ class ActionsTest extends \PHPUnit\Framework\TestCase
                 $promise = Actions\Action::_queryParams($action, $params, $urlParams);
 
                 $this->assertInternalType('array', $promise);
+            });
+    }
+
+    /**
+     * @eris-repeat 5
+     */
+    public function testCreateIndexGeneratesDatabaseSearchQueryIndex()
+    {
+        $this->forAll(
+            Generator\constant('testdb'),
+            Generator\associative([
+                'index' => Generator\associative([
+                    'fields' => Generator\elements('_id', '_rev', 'foo')
+                ]),
+                'name' => Generator\names('foo-index', 'bar-index')
+            ])
+        )
+            ->then(function (string $database, array $opts) {
+                $promise = $this->action->createIndex($database, $opts);
+                $index = self::blockFn()($promise, $this->eventLoop);
+
+                $this->assertInstanceOf(\React\Promise\Promise::class, $promise);
+                $this->assertInternalType('string', $index);
+            });
+    }
+
+    /**
+     * @eris-repeat 5
+     */
+    public function testGetIndexesOutputsIndexInformation()
+    {
+        $this->forAll(Generator\constant('testdb'))
+            ->then(function (string $database) {
+                $promise = $this->action->getIndexes($database);
+                $indexes = self::blockFn()($promise, $this->eventLoop);
+
+                $this->assertInstanceOf(\React\Promise\Promise::class, $promise);
+                $this->assertInternalType('string', $indexes);
+            });
+    }
+
+    /**
+     * @eris-repeat 5
+     */
+    public function testChangesFunctionOutputsDatabaseChangeLogData()
+    {
+        $this->forAll(
+            Generator\constant('testdb'),
+            Generator\associative([
+                'descending' => Generator\elements('true', 'false'),
+                'include_docs' => Generator\elements('true', 'false'),
+                'conflicts' => Generator\elements('true', 'false'),
+                'limit' => Generator\choose(1, 5)
+            ])
+        )
+            ->then(function (string $database, array $params) {
+                $promise = $this->action->changes($database, $params);
+                $changes = self::blockFn()($promise, $this->eventLoop);
+
+                $this->assertInstanceOf(\React\Promise\Promise::class, $promise);
+                $this->assertInternalType('string', $changes);
+            });
+    }
+
+    /**
+     * @eris-repeat 5
+     * @eris-ratio 0.1
+     */
+    public function testUpdateSingleUpdatesSingleDocumentInDatabase()
+    {
+        $this->forAll(
+            Generator\constant('testdb'),
+            Generator\suchThat(self::revConst, Generator\string()),
+            Generator\suchThat(self::idConst, Generator\string()),
+            Generator\associative([
+                'age' => Generator\choose(18, 70),
+                'date' => Generator\date()
+            ])
+        )
+            ->then(function (string $database, string $rev, string $docId, array $update) {
+                $promise = $this->action->updateSingle($database, $rev, $docId, $update);
+                $update = self::blockFn()($promise, $this->eventLoop);
+
+                $this->assertInstanceOf(\React\Promise\Promise::class, $promise);
+                $this->assertInternalType('string', $update);
+            });
+    }
+
+    /**
+     * @eris-repeat 5
+     * @eris-ratio 0.1
+     */
+    public function testUpdateMultipleUpdatesMultipleDocumentsInDatabase()
+    {
+        $this->forAll(
+            Generator\constant('testdb'),
+            Generator\associative([
+                'docs' => Generator\tuple(
+                        Generator\associative([
+                            '_id' => Generator\suchThat(self::idConst, Generator\string()),
+                            '_rev' => Generator\suchThat(self::revConst, Generator\string()),
+                            'name' => Generator\names()
+                        ])
+                    )
+            ])
+        )
+            ->then(function (string $database, array $update) {
+                $promise = $this->action->updateMultiple($database, $update);
+                $update = self::blockFn()($promise, $this->eventLoop);
+
+                $this->assertInstanceOf(\React\Promise\Promise::class, $promise);
+                $this->assertInternalType('string', $update);
+            });
+    }
+
+    /**
+     * @eris-repeat 5
+     * @eris-ratio 0.1
+     */
+    public function testDeleteSingleDeletesSingleDocument()
+    {
+        $this->forAll(
+            Generator\constant('testdb'),
+            Generator\suchThat(self::revConst, Generator\string()),
+            Generator\suchThat(self::idConst, Generator\string())
+        )
+            ->then(function (string $database, string $rev, string $docId) {
+                $promise = $this->action->deleteSingle($database, $rev, $docId);
+                $delete = self::blockFn()($promise, $this->eventLoop);
+
+                $this->assertInstanceOf(\React\Promise\Promise::class, $promise);
+                $this->assertInternalType('string', $delete);
+            });
+    }
+
+    /**
+     * @eris-repeat 5
+     * @eris-ratio 0.1
+     */
+    public function testDeleteMultipleDeletesMultipleDocuments()
+    {
+        $this->forAll(
+            Generator\constant('testdb'),
+            Generator\associative([
+                'docs' => Generator\tuple(
+                    Generator\associative([
+                        '_id' => Generator\suchThat(self::idConst, Generator\string()),
+                        '_rev' => Generator\suchThat(self::revConst, Generator\string())
+                    ])
+                )
+            ])
+        )
+            ->then(function (string $database, array $data) {
+                $promise = $this->action->deleteMultiple($database, $data);
+                $delete = self::blockFn()($promise, $this->eventLoop);
+
+                $this->assertInstanceOf(\React\Promise\Promise::class, $promise);
+                $this->assertInternalType('string', $delete);
+            });
+    }
+
+    /**
+     * @eris-repeat 5
+     */
+    public function testCreateDesignDocCreatesDesignDocuments()
+    {
+        $this->forAll(
+            Generator\constant('testdb'),
+            Generator\elements('id-designdoc', 'rev-designdoc'),
+            Generator\associative([
+                'views' => Generator\associative([
+                    'map' => Generator\elements(
+                        'function (doc) { emit(doc._id); }', 
+                        'function (doc) { emit(doc._rev, doc.user); }'
+                    )
+                ]) 
+            ])
+        )
+            ->then(function (string $database, string $ddoc, array $docData) {
+                $promise = $this->action->createDesignDoc($database, $ddoc, $docData);
+                $ddoc = self::blockFn()($promise, $this->eventLoop);
+
+                $this->assertInstanceOf(\React\Promise\Promise::class, $promise);
+                $this->assertInternalType('string', $ddoc);
             });
     }
 }
